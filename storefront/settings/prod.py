183554c1cf90189be136f68d194dd8c7574f96db
Raks-Javac <rufaikudus2014@gmail.com>
2025-04-12 02:54:23 +0100

    Add dj-database-url dependency and update production database settings

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,6 +1,6 @@
 from .common import *
 
-
+import dj_database_url
 
 from .common import *
 import os
@@ -18,14 +18,23 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront3',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'rjcs_javac'
+    'default':{
+                'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
+        'NAME': os.getenv('DB_NAME'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PORT': os.getenv('DB_PORT', '3306'),  # Optional if default
     }
 }
+
+#    {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'storefront3',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'rjcs_javac'
+#     }
 
 
 ALLOWED_HOSTS = ['store-app-v1.onrender.com']
